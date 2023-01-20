1c7cd85d9b2902a23100b4024293edea22011c5f
Sami1150 <bsef19m045@pucit.edu.pk>
2023-01-21 04:05:56 +0500

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,20 @@
+﻿public class Solution
+{
+    public bool IsPalindrome(int x)
+    {
+        string temp = "";
+        string val = x.ToString();
+        for (int i = val.Length - 1; i >= 0; i--)
+        {
+            temp = temp + val[i];
+        }
+
+        if (temp == val)
+        {
+            return true;
+        }
+        else
+            return false;
+
+    }
+}
